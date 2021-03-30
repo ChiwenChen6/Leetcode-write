b07fb6599a935b147b5e0ad369b76a671622eba0
ChiwenChen6 <43346213+ChiwenChen6@users.noreply.github.com>
2021-03-30 10:52:55 +0800

    Update 971 Flip Binary Tree To Match Preorder Traversal.java

--- 971 Flip Binary Tree To Match Preorder Traversal.java
+++ 971 Flip Binary Tree To Match Preorder Traversal.java
@@ -53,3 +53,28 @@ class Solution {
         }
     }
 }
+
+
+
+// 2nd solution
+class Solution {
+    int vix = 0;
+    List<Integer> ans = new ArrayList<>();
+    private void dfs(TreeNode node, int[] V) {
+        if (node == null || (ans.size() != 0 && ans.get(0) == -1)) return;
+        if (node.val != V[vix++])
+            ans = new ArrayList<Integer>(Arrays.asList(-1));
+        else if (node.left != null && node.left.val != V[vix]) {
+            ans.add(node.val);
+            dfs(node.right, V);
+            dfs(node.left, V);
+        } else {
+            dfs(node.left, V);
+            dfs(node.right, V);
+        }
+    }
+    public List<Integer> flipMatchVoyage(TreeNode root, int[] V) {
+        dfs(root, V);
+        return ans;
+    }
+}
