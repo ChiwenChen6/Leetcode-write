f925b36f6f67636d037c0047ff1030a8fca47b5e
ChiwenChen6 <43346213+ChiwenChen6@users.noreply.github.com>
2021-03-30 11:05:57 +0800

    Create 971 Flip Binary Tree To Match Preorder Traversal.js

---
+++ 971 Flip Binary Tree To Match Preorder Traversal.js
@@ -0,0 +1,22 @@
+const flipMatchVoyage = function (root, V) {
+    let ans = [], vix = 0
+    const dfs = node => {
+        if (!node || ans[0] === -1) return
+        if (node.val !== V[vix++]) ans = [-1]
+        else if (node.left && node.left.val !== V[vix]) {
+            ans.push(node.val)
+            dfs(node.right)
+            dfs(node.left)
+        } else {
+            dfs(node.left)
+            dfs(node.right)
+        }
+    }
+    dfs(root)
+    return ans
+};
+
+
+// Status: Accepted
+// Runtime: 0 ms
+// Memory Usage: 38.9 MB
