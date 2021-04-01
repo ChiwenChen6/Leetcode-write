1f14a2efa3cab8c2319eafbc9e59b235d31927e9
ChiwenChen6 <43346213+ChiwenChen6@users.noreply.github.com>
2021-04-01 15:55:18 +0800

    Add 234 Palindrome Linked List.c

---
+++ 234 Palindrome Linked List.c
@@ -0,0 +1,46 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+struct ListNode* reverse(struct ListNode* head){
+    if(head == NULL)
+        return NULL;
+    struct ListNode* curr = head;
+    struct ListNode* prev = NULL;
+    struct ListNode* NextNode = NULL;
+
+    while(curr){
+        NextNode = curr -> next;
+        curr-> next = prev;
+        prev = curr;
+        curr = NextNode;
+    }
+    return prev;
+}
+
+bool isPalindrome(struct ListNode* head) {
+
+    if(head == NULL)
+        return true;
+    struct ListNode* fast = head;
+    struct ListNode* slow = head; 
+
+    while(fast -> next != NULL && fast -> next -> next != NULL){
+        slow = slow -> next;
+        fast = fast -> next ->next;
+    }
+    slow -> next = reverse(slow -> next);
+    slow = slow -> next;
+
+    while(slow != NULL){
+        if(head -> val != slow -> val)
+            return false;
+        slow = slow -> next;
+        head = head -> next;
+    }
+    return true;
+}
