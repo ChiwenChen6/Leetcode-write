e0ee1b61db8fd6fb51c3fd61b235f5c218641c9a
ChiwenChen6 <43346213+ChiwenChen6@users.noreply.github.com>
2021-04-08 11:06:29 +0800

    Create 1704 Determine if String Halves Are Alike.go

---
+++ 1704 Determine if String Halves Are Alike.go
@@ -0,0 +1,19 @@
+func halvesAreAlike(s string) bool {
+    l := len(s)
+    x1 := 0
+    x2 := 0
+    for pos, x := range s{
+        if pos < l/2{
+            switch x{
+                case 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U':
+                    x1++
+            }
+        }else{
+            switch x{
+                case 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U':
+                    x2++
+            }
+        }
+    }
+    return x1 == x2
+}
