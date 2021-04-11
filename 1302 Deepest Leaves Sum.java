f436dbaaeed4342dc6f350549b4386fbb80de9c5
ChiwenChen6 <43346213+ChiwenChen6@users.noreply.github.com>
2021-04-12 00:23:47 +0800

    Create 1302 Deepest Leaves Sum.java

---
+++ 1302 Deepest Leaves Sum.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int deepestLeavesSum(TreeNode root) {
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        int ans = 0, qlen = 0;
+        while (q.size() > 0) {
+            qlen = q.size();
+            ans = 0;
+            for (int i = 0; i < qlen; i++) {
+                TreeNode curr = q.poll();
+                ans += curr.val;
+                if (curr.left != null) q.add(curr.left);
+                if (curr.right != null) q.add(curr.right);
+            }
+        }
+        return ans;
+    }
+}
+// Status: Accepted
+// Runtime: 3 ms
+// Memory Usage: 39.9 MB
