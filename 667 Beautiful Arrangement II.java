6030383cff522987db37819b7758ae38a3a90290
ChiwenChen6 <43346213+ChiwenChen6@users.noreply.github.com>
2021-04-13 10:11:36 +0800

    Create 667 Beautiful Arrangement II.java

---
+++ 667 Beautiful Arrangement II.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] constructArray(int n, int k) {
+        int[] ans = new int[n];
+        for (int i = 0, a = 1, z = k + 1; i <= k; i++)
+            ans[i] = i % 2 == 1 ? z-- : a++;
+        for (int i = k+1; i < n;)
+            ans[i] = ++i;
+        return ans;
+    }
+}
+// Status: Accepted
+// Runtime: 0 ms
+// Memory Usage: 39.1 MB
