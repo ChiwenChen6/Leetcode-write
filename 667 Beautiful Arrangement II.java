84f6afbc7573114f525ffd73cec385cec802aa6a
ChiwenChen6 <43346213+ChiwenChen6@users.noreply.github.com>
2021-04-13 10:13:39 +0800

    Update 667 Beautiful Arrangement II.java

--- 667 Beautiful Arrangement II.java
+++ 667 Beautiful Arrangement II.java
@@ -1,10 +1,12 @@
 class Solution {
     public int[] constructArray(int n, int k) {
         int[] ans = new int[n];
-        for (int i = 0, a = 1, z = k + 1; i <= k; i++)
+        for (int i = 0, a = 1, z = k + 1; i <= k; i++) {
             ans[i] = i % 2 == 1 ? z-- : a++;
-        for (int i = k+1; i < n;)
+        }
+        for (int i = k+1; i < n;) {
             ans[i] = ++i;
+        }
         return ans;
     }
 }
