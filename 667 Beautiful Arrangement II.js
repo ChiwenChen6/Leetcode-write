8ec488b691592ce159fb3631cb6e5ca4756a8efd
ChiwenChen6 <43346213+ChiwenChen6@users.noreply.github.com>
2021-04-13 10:12:53 +0800

    Update 667 Beautiful Arrangement II.js

--- 667 Beautiful Arrangement II.js
+++ 667 Beautiful Arrangement II.js
@@ -5,10 +5,12 @@
  */
 var constructArray = function(n, k) {
     let ans = new Array(n)
-    for (let i = 0, a = 1, z = k + 1; i <= k; i++)
+    for (let i = 0, a = 1, z = k + 1; i <= k; i++) {
         ans[i] = i % 2 ? z-- : a++
-    for (let i = k + 1; i < n;)
+    }
+    for (let i = k + 1; i < n;) {
         ans[i] = ++i
+    }
     return ans
 };
 
