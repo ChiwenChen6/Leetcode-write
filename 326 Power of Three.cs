5161e171d3bffdfada83c36dbbdcd5620976bb21
ChiwenChen6 <43346213+ChiwenChen6@users.noreply.github.com>
2021-04-28 14:35:18 +0800

    Create 326 Power of Three.cs

---
+++ 326 Power of Three.cs
@@ -0,0 +1,7 @@
+public class Solution {
+    public bool IsPowerOfThree(int n) {
+        return (n > 0 && 1162261467 % n == 0);      // 3 ^ 19 = 1162261467
+    }
+}
+
+***04/28/2021 14:34	Accepted	84 ms	17.1 MB	csharp
