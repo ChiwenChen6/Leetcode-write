ee448d90313733e461f5316991965d7079d0f4df
ChiwenChen6 <43346213+ChiwenChen6@users.noreply.github.com>
2021-05-04 19:58:33 +0800

    Create 665 Non-decreasing Array.java

---
+++ 665 Non-decreasing Array.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean checkPossibility(int[] nums) {
+        for (int i = 1, err = 0; i < nums.length; i++) {
+            if (nums[i] < nums[i-1]) {
+                if (err++ > 0 || (i > 1 && i < nums.length - 1 && nums[i-2] > numsN[i] && nums[i+1] < nums[i-1])) {
+                    return false;
+                }
+        return true;
+            }
+        }
+    }
+}
