74324faef043cc54dc818cc038290e18a549d7a3
ChiwenChen6 <43346213+ChiwenChen6@users.noreply.github.com>
2021-08-30 15:29:46 +0800

    Create 598 Range Addition II.c

---
+++ 598 Range Addition II.c
@@ -0,0 +1,15 @@
+
+
+int maxCount(int m, int n, int** ops, int opsSize, int* opsColSize){
+
+	for(int i = 0; i < opsSize; i++) {
+		if(ops[i][0] < m) {
+			m = ops[i][0];
+		}
+		if(ops[i][1] < n) {
+			n = ops[i][1];
+		}
+	}
+	return m * n;
+
+}
