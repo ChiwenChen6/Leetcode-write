847315aed49030b389cce0de60e93d6ce9a1eed1
ChiwenChen6 <43346213+ChiwenChen6@users.noreply.github.com>
2021-08-31 17:10:18 +0800

    Create 153 Find Minimum in Rotated Sorted Array.kt

---
+++ 153 Find Minimum in Rotated Sorted Array.kt
@@ -0,0 +1,8 @@
+class Solution {
+    fun findMin(nums: IntArray): Int {
+        return nums.min()!!
+    }
+}
+
+// Runtime: 200 ms, faster than 27.18% of Kotlin online submissions for Find Minimum in Rotated Sorted Array.
+// Memory Usage: 38.1 MB, less than 9.71% of Kotlin online submissions for Find Minimum in Rotated Sorted Array.
