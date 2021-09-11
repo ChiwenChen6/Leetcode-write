756e877d77cc449615c64cd731263f39160ae453
ChiwenChen6 <43346213+ChiwenChen6@users.noreply.github.com>
2021-09-11 16:07:08 +0800

    Create 292 Nim Game.c

---
+++ 292 Nim Game.c
@@ -0,0 +1,8 @@
+
+
+bool canWinNim(int n) {
+    return n % 4 != 0;
+}
+
+Runtime: 0 ms, faster than 100.00% of C online submissions for Nim Game.
+Memory Usage: 5.4 MB, less than 93.81% of C online submissions for Nim Game.
