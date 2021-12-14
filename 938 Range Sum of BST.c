157b196d65fa6e99811e8b8d9b7c2ebd381a1548
ChiwenChen6 <43346213+ChiwenChen6@users.noreply.github.com>
2021-12-14 13:15:29 +0800

    Create 938 Range Sum of BST.c

---
+++ 938 Range Sum of BST.c
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+
+int rangeSumBST(struct TreeNode* root, int low, int high){
+    if (!root) {
+        return 0;
+    }
+    if(root->val<low){
+        return rangeSumBST(root->right, low, high);
+    }
+    if(root->val>high){
+        return rangeSumBST(root->left, low, high);
+    }
+    return root->val + rangeSumBST(root->left, low, high) + rangeSumBST(root->right, low, high);
+}
