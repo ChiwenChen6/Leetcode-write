fc01322742f5a20e898a4fea11821ac064ad1196
ChiwenChen6 <43346213+ChiwenChen6@users.noreply.github.com>
2021-12-15 15:04:42 +0800

    Create 147 Insertion Sort List.java

---
+++ 147 Insertion Sort List.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode insertionSortList(ListNode head) {
+        if (head == null) return head;
+        ListNode root = head;
+        ListNode current = head.next;
+        ListNode beforeCurrent = head;
+        
+        while (current != null) {
+            if (current.val >= beforeCurrent.val) {
+                beforeCurrent = current;
+                current = current.next;
+                continue;
+            } else {
+                if (root.val > current.val) {
+                    beforeCurrent.next = current.next;
+                    current.next = root;
+                    root = current;
+                    current = beforeCurrent.next;
+                    continue;
+                }
+                ListNode searchBy = root;
+                while (searchBy.next.val < current.val) {
+                    searchBy = searchBy.next;
+                }
+                beforeCurrent.next = current.next;
+                current.next = searchBy.next;
+                searchBy.next = current;
+                current = beforeCurrent.next;
+            }
+        }
+        return root;
+    }
+}
