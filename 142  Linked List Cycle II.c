d7a811595b9ea3fbc5c8cfa6e68bed25adac686d
ChiwenChen6 <43346213+ChiwenChen6@users.noreply.github.com>
2022-01-19 15:33:53 +0800

    Update 142  Linked List Cycle II.c

--- 142  Linked List Cycle II.c
+++ 142  Linked List Cycle II.c
@@ -1,19 +1,26 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
-
-class Solution:
-    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        l = []
-        if not head:
-            return None
-        node = head
-        while node.next:
-            if id(node) in l:
-                return node
-            else:
-                l += [id(node)]
-                node = node.next
-        return None
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode *detectCycle(struct ListNode *head) {
+    if (head == NULL || head->next == NULL)
+        return NULL;
+    struct ListNode *slow, *fast;
+    slow = fast = head;
+    while(fast && fast->next) {
+        slow = slow->next;
+        fast = fast->next->next;
+        if(slow == fast) {
+            struct ListNode *entry = head;
+            while(slow != entry) {
+                slow = slow -> next;
+                entry = entry -> next;
+            }
+            return entry;
+        }
+    }
+    return NULL;
+}
