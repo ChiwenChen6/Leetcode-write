02378d14d28db53c53ccc9bf4512ba7ac9b2080a
ChiwenChen6 <43346213+ChiwenChen6@users.noreply.github.com>
2022-02-08 15:47:07 +0800

    Create 258 Add Digits.go

---
+++ 258 Add Digits.go
@@ -0,0 +1,11 @@
+func addDigits(num int) int {
+    for num > 9 {
+        cur := 0
+        for num != 0 {
+            cur += num % 10
+            num /= 10
+        }
+        num = cur
+    }
+    return num
+}
