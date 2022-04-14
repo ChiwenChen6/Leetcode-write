b328633b2e4450f8db0eef726e2ad1bd1aea1395
ChiwenChen6 <43346213+ChiwenChen6@users.noreply.github.com>
2022-04-14 13:31:35 +0800

    Create 700 Search in a Binary Search Tree.c

---
+++ 700 Search in a Binary Search Tree.c
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+
+struct TreeNode* searchBST(struct TreeNode* root, int val){
+    if (root==NULL) {
+        return NULL;
+    }
+    if (root->val==val) { 
+        return root;
+    }
+    if (root->val < val) {      
+        return searchBST(root->right,val);
+    }    
+    else return searchBST(root->left,val);
+
+}
